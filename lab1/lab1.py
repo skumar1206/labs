2d08b7f0489b87941fca5d31c87d21cf1087199b
Siddhant Kumar <sid@Siddhants-Air.lan>
2023-09-18 17:18:38 -0600

    feat: modified script to print its own source code

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -2,3 +2,6 @@ import requests
 homepage = requests.get("http://www.google.com/")
 
 print(homepage.status_code)
+
+raw_script = requests.get("https://raw.githubusercontent.com/skumar1206/labs/main/lab1/lab1.py")
+print(raw_script.text)
